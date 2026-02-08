1c6ccb64fc776eb1307f9e16bbe039629018048f
Jon Andre Briones <jonbrsoftware@gmail.com>
2026-02-08 03:03:50 -0800

    feat(github): set up to host on github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,8 @@ import icon from 'astro-icon'; // https://www.astroicon.dev/guides/upgrade/v1/
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://sprintthesummit.ca',
+  site: 'https://jbriones1.github.io',
+  base: '/sprint-the-summit',
   integrations: [
     // example auto import component into blog post mdx files
     // AutoImport({
